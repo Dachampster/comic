e0bcbd61b5900f3ca45cf81224ba7f599841631b
Randolph <ampsternetwork@gmail.com>
2018-05-29 18:32:56 -0400

    gggg

--- models/orm.js
+++ models/orm.js
@@ -1,46 +1,48 @@
 const fs = require('fs');
 var path = require('path');
-fs.readFile('config/seed.json', 'utf8', function(err,data){
-    if (err){
+fs.readFile('config/seed.json', 'utf8', function (err, data) {
+    if (err) {
         throw err;
     }
     const comics = JSON.parse(data);
-   console.log(orm.find(comics,comics[100],'backward'));
+    console.log(orm.find(comics, comics[199], 'backwards'));
+    console.log(orm.grabIndex(comics, 10));
+
 });
 
-// orm.grabIndex(comics, comics.length, function(page) {
-//     console.log(page.src);
-// });
-
 const orm = {
-    grabIndex : function(comics, lookup) {
-        for (index in comics) {
-            if (index == lookup - 1) {
-               return comics[index];
-            } else if (comics[index] instanceof Object) {
-                grabIndex(comics[index], lookup);
+    // comics : this.comics,
+    grabIndex: function (comics, lookup) {
+        for (chapter in comics) {
+            if (chapter == lookup) {
+                return comics[chapter];
+            } else if (comics[chapter] instanceof Object) {
+                grabIndex(comics[chapter], lookup);
             }
         }
     },
-    grabFirst : function(comics){
+    grabFirst: function (comics) {
         return comics[0];
     },
-    grabLatest : function(comics){
+    grabLatest: function (comics) {
         return comics[comics.length];
     },
 
-    find : function(comic,current, direction) {
+    find: function (comic, current, direction) {
+        console.log(current);
         let index = current.index;
-        switch (direction){
-            case 'forward':
-            newPage = comic[index + 1] || comic[index];
-            break;
-            case 'backward':
-            newPage = comic[index - 1] || comic[index];
-            break;
+        if (direction == 'forwards') {
+
+            lookup = comic[index] || comic[index - 1];
         }
-        return this.grabIndex(newPage);
+        else if (direction == 'backwards') {
+
+            lookup = comic[index - 2] || comic[index - 1];
+
+        }
+        return (lookup);
     }
-    
+
 }
+
 module.exports = orm;
